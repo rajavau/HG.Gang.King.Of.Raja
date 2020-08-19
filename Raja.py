509fa04a986c70ac3aef4b0a661d7ce3bcf34fdf
rajavau <69924288+rajavau@users.noreply.github.com>
2020-08-20 05:23:46 +0600

    Create Raja.py

---
+++ Raja.py
@@ -0,0 +1,10 @@
+# target-account-COMMNAD
+apt update 
+apt upgrade 
+pkg install python 
+pkg install python2
+ pip2 install requests 
+pip2 install mechanize 
+pkg install git 
+git clone https://github.com/rajavau/HG.Gang.King.Of.Raja.git
+python2 raja.py
